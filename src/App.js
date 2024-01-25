98e3c2cc4b8c6af03f936eefdd54f8a7b5507c6b
omprakash1907 <omprakashkjat19@gmail.com>
2024-01-25 11:25:04 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Crud from './Realtime-firestore/Crud';
+import User from './Realtime-firestore/User';
+
+
+function App() {
+  return (
+    <User/>
+  );
+}
+
+export default App;
